645062b493403997a2ab622f71d7c85dee7fc15f
alivasahu <aliva.sahu@thesunflowerlab.com>
2023-10-17 15:57:41 +0530

    second commit

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -7,12 +7,12 @@ export default function Sidebar() {
     <div className='sidebar'>
         <h2 className='heading'><FaHome className='icon'/><span>Dashboard</span></h2>
         <ul>
-        <li className='list active'><FaHome className='icon'/><span>Dashboard</span></li>
-            <li className='list'><FaProductHunt className='icon'/><span>Product</span></li>
-            <li className='list'><FaUserCircle className='icon'/><span>Customer</span></li>
-            <li className='list'><FaRupeeSign className='icon'/><span>Income</span></li>
-            <li className='list'><FaUserCircle className='icon'/><span>Promote</span></li>
-            <li className='list'><FaHireAHelper className='icon'/><span>Help</span></li>
+        <li className='list active'><a href="#"><FaHome className='icon'/><span>Dashboard</span></a></li>
+            <li className='list'><a href="#"><FaProductHunt className='icon'/><span>Product</span></a></li>
+            <li className='list'><a href="#"><FaUserCircle className='icon'/><span>Customer</span></a></li>
+            <li className='list'><a href="#"><FaRupeeSign className='icon'/><span>Income</span></a></li>
+            <li className='list'><a href="#"><FaUserCircle className='icon'/><span>Promote</span></a></li>
+            <li className='list'><a href="#"><FaHireAHelper className='icon'/><span>Help</span></a></li>
         </ul>
         <div className="last-section list">
             <img src="./client1.png" alt="" />
